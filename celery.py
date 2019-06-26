ee6de925262876445c6dec7e87f32fa80a093a8f
Vladyslav <vmash@softserveinc.com>
2019-06-26 17:41:47 +0300

    Custom routing, beat

--- celery.py
+++ celery.py
@@ -4,5 +4,6 @@ from celery import Celery
 app = Celery('trysanic')
 app.config_from_object('trysanic.celeryconfig')
 
+
 if __name__ == '__main__':
     app.start()
--- celeryconfig.py
+++ celeryconfig.py
@@ -1,13 +1,28 @@
+from kombu import Queue
+
+
 broker_url='pyamqp://guest@localhost//'
 result_backend='redis://localhost'
 
-include = ['trysanic.tasks']
+task_default_queue = 'default'
+tasks_queues = (
+        Queue('default', routing_key='task.#'),
+        Queue('feed_tasks', routing_key='feed.#'),
+)
+task_default_exchange = 'tasks'
+task_default_exchange_type = 'topic'
+task_default_routing_key = 'task.default'
+
+imports = ['trysanic.tasks']
 
 task_routes = {
-    'tasks.add': 'high'
+        'trysanic.tasks.add': { 'queue': 'high', 'routing_key': 'feed.import'}
 }
 
 task_annotations = {
     'tasks.add': { 'rate_limit': '1/m' }
 }
 
+beat_schedule = {
+    'test': {'task': 'trysanic.tasks.add', 'schedule': 10.0, 'args': (4, 4)}
+}
